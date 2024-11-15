8778ab991e61d799ce694679c1f5d11e3b7cd04c
Yash Gandhi <ygandhi3@stevens.edu>
2024-11-15 15:42:31 -0500

    adding bid model

---
+++ backend/models/bid.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const bidSchema = new Schema({
+    projectId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Project',
+        required: true
+    },
+    clientId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    freelancerId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    bidAmount: {
+        type: Number,
+        required: [true, "Bid amount can't be empty!"],
+        min: [1, "Bid amount should be greater than 0"]
+    },
+    proposal: {
+        type: String,
+        required: [true, "Proposal can't be empty!"],
+        minlength: [10, "Proposal should be at least 10 characters long"]
+    },
+    isApproved: {
+        type: Boolean,
+        default: false
+    }
+}, {
+    timestamps: true
+});
+
+export default mongoose.model("Bid", bidSchema)
