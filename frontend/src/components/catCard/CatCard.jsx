217a8abdd27635301deba88f0b93115dd7fb584a
keyur-patel-21 <keyurspatel2023@gmail.com>
2024-11-17 15:44:23 -0500

    implment slides for category

---
+++ frontend/src/components/catCard/CatCard.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./CatCard.css";
+
+function CatCard({ card }) {
+  return (
+    <Link to="/gigs?cat=design">
+      <div className="catCard">
+        <img src={card.img} alt={card.title} />
+        <div className="info">
+          <span className="desc">{card.desc}</span>
+          <span className="title">{card.title}</span>
+        </div>
+      </div>
+    </Link>
+  );
+}
+
+export default CatCard;
