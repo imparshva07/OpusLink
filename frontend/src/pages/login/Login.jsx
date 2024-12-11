f3bdc089b7fb3a3cb31ea045acef19538c72959e
keyur-patel-21 <keyurspatel2023@gmail.com>
2024-12-10 23:38:01 -0500

    redesign login page

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -9,40 +9,46 @@ function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [user, loading, error] = useAuthState(auth);
-  
-  const navigate = useNavigate();
 
-  // TODO *************
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (loading) return;
     if (user) navigate("/");
-  }, [user, loading]);
+  }, [user, loading, navigate]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Prevent the form from refreshing the page
+    logInWithEmailAndPassword(email, password);
+  };
 
   return (
     <div className="login">
-      <div className="login_box">
+      <form onSubmit={handleSubmit}>
+        <h1>Sign In</h1>
+
+        <label htmlFor="email">Email</label>
         <input
+          id="email"
           type="text"
-          className="login_einput"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
-          placeholder="E-mail Address"
+          placeholder="Enter Your Email Address"
         />
+
+        <label htmlFor="password">Password</label>
         <input
+          id="password"
           type="password"
-          className="login_pinput"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           placeholder="Password"
         />
-        <button
-          className="login_btn"
-          // TODO *************
-          onClick={() => logInWithEmailAndPassword(email, password)}
-        >
-          Login
-        </button>
+
+        <button type="submit">Login</button>
+
+        {error && <div className="error-message">{error.message}</div>}
+
         <div className="extra_options">
           <div>
             <Link to="/reset">Forgot Password</Link>
@@ -51,7 +57,7 @@ function Login() {
             Don't have an account? <Link to="/register">Register</Link> now.
           </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
