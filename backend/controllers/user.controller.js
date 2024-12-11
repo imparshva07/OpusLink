0f521a3053e2c2980c63797f24db5a1ba5bc8886
imparshva07 <parshvashah2014@gmail.com>
2024-12-11 02:00:26 -0500

    fixing codebase

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,45 @@
+import {User} from "../models/user.model.js"
+
+export const deleteUser = async (req, res) => {
+  await User.findByIdAndDelete(req.params.id);
+}
+
+export const getUser = async (req, res) => {
+  const { id } = req.params
+  try {
+    const user = await User.findById(id)
+    if (!user) {
+      throw "user does not exist!"
+    }
+    else {
+      return res.status(200).json(user)
+    }
+  } catch (e) {
+    if (e === "user does not exist!") {
+      return res.status(404).json({ error: e });
+    }
+    return res.status(500).json({ error: e });
+  }
+}
+
+export const updateUser = async (req, res) => {
+  const { id } = req.params
+  try {
+    const updatedUser = await User.findByIdAndUpdate(id, req.body, { new: true })
+    if (!updatedUser) {
+      return res.status(404).json({ error: "User not found!" });
+    }
+    return res.status(200).json(updatedUser)
+  } catch (e) {
+    return res.status(500).json({ error: e });
+  }
+}
+
+export const getAll = async (req, res) => {
+  try {
+    const allUsers = await User.find();
+    return res.status(200).json(allUsers)
+  } catch (e) {
+    return res.status(500).json({ error: e });
+  }
+}
--- backend/controllers/user.js
+++
@@ -1,6 +0,0 @@
-import User from "../models/user.js";
-
-
-export const deleteUser = async (req, res) => {
-  await User.findByIdAndDelete(req.params.id);
-};
