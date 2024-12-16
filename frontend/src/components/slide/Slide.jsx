5b26b1025df2551518d0c3aa54026f416a6d204f
imparshva07 <parshvashah2014@gmail.com>
2024-12-16 05:31:15 -0500

    Dashboard updates with Project search and display functionality for client and freelancer

--- frontend/src/components/slide/Slide.jsx
+++ frontend/src/components/slide/Slide.jsx
@@ -1,8 +1,10 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
 import React from "react";
 import "./Slide.css";
 import Slider from "infinite-react-carousel";
 
-const Slide = ({ children, slidesToShow, arrowsScroll }) => {
+const Slide = ({ children, slidesToShow, arrowsScroll, projectData }) => {
   return (
     <div className="slide">
       <div className="container">
